339ee46b222192c02ba8b5e3f43c48369f7d85ea
Nikolozgvaramia <117025467+Nikolozgvaramia@users.noreply.github.com>
2023-02-13 20:34:50 +0400

    Add files via upload

---
+++ experience/experience.js
@@ -0,0 +1,17 @@
+const continueBtn = document.getElementById("continue-btn")
+
+continueBtn.onclick = () => {
+    location.href = "./experience1.html"
+}
+
+
+const positionLabel = document.getElementById("position-label");
+const positionInput = document.getElementById("position-input");
+const positionP = document.getElementById("position-p");
+const employerLabel = document.getElementById("employer-label");
+const employerInput = document.getElementById("employer-input")
+const employerP = document.getElementById("employer-p")
+
+
+
+
